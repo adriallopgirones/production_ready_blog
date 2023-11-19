4c321979ddd93594e2eee6570fa8ac301b814585
adriallopgirones <adriallopgirones@gmail.com>
2023-11-19 17:46:59 +0100

    Initial Architecture and django structure created

---
+++ src/production_ready_blog_project/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *  # noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# Ran python -c "import secrets; print(secrets.token_urlsafe(38))" to obtain a new secret key
+SECRET_KEY = (
+    env(
+        "DJANGO_SECRET_KEY",
+        default="DvMUU3Taijp4oVLVSOVqvQ72dEkRw2hTe80Av6vYu3mG4dFxmo0",
+    ),
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# TODO: Use CRFS
+ALLOWED_HOSTS = ["http://localhost:8000"]
